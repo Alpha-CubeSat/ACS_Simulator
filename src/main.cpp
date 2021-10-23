ba2324d97954add646a67ea99b6981bb0bf5d0cc
Lauren Greenhill <ljgreenhill3@gmail.com>
2021-10-22 20:33:13 -0400

    added delay code

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,43 @@
 
 static Plantv50ModelClass plantObj;
 static StarshotACS0ModelClass starshotObj;
+int start_time = 0;
+int imu_delay = 500;
 
 void setup(){  
   plantObj.initialize();
   starshotObj.initialize();
+  start_time = millis();
 }
 
 void loop(){
-  starshotObj.step();
 
-  Serial.println("Angular Velocity p");
-  Serial.println(starshotObj.rtU.w[0]);
-  Serial.println("Angular Velocity q");
-  Serial.println(starshotObj.rtU.w[1]);
-  Serial.println("Angular Velocity r");
-  Serial.println(starshotObj.rtU.w[2]);
+
+  starshotObj.step();
 
   plantObj.rtU.current[0] = starshotObj.rtY.detumble[0];
   plantObj.rtU.current[1] = starshotObj.rtY.detumble[1];
   plantObj.rtU.current[2] = starshotObj.rtY.detumble[2];
 
-  plantObj.step();
+  Serial.print(millis() - start_time);
+  Serial.print(",");
+  Serial.print(starshotObj.rtU.w[0]);
+  Serial.print(",");
+  Serial.print(starshotObj.rtU.w[1]);
+  Serial.print(",");
+  Serial.print(starshotObj.rtU.w[2]);
+  Serial.print(",");
+  Serial.print(plantObj.rtU.current[0]);
+  Serial.print(",");
+  Serial.print(plantObj.rtU.current[1]);
+  Serial.print(",");
+  Serial.println(plantObj.rtU.current[2]);
 
+  while(millis()-start_time <= imu_delay){
+    plantObj.step();
+  }
+  start_time = millis();
+  
   starshotObj.rtU.w[0] = plantObj.rtY.angularvelocity[0];
   starshotObj.rtU.w[1] = plantObj.rtY.angularvelocity[1];
   starshotObj.rtU.w[2] = plantObj.rtY.angularvelocity[2];

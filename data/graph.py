15b912c02eaead05e0463ea4916dafc24ce88e17
Lauren Greenhill <ljgreenhill3@gmail.com>
2021-10-31 21:37:38 -0400

    more data

--- data/graph.py
+++ data/graph.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
 import csv
-  
+
 time = []
 av_p = []
 av_q = []
 av_r = []
-  
-with open('3msimu1plantsim.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter = ',')
-      
+
+with open('500delay.csv', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+
     for row in plots:
         time.append(float(row[0]))
         av_p.append(float(row[1]))
         av_q.append(float(row[2]))
         av_r.append(float(row[3]))
-  
-plt.plot(time, av_p, label = "P Angular Velocity")
-plt.plot(time, av_q, label = "Q Angular Velocity")
-plt.plot(time, av_r, label = "R Angular Velocity")
+
+plt.plot(time, av_p, label="P Angular Velocity")
+plt.plot(time, av_q, label="Q Angular Velocity")
+plt.plot(time, av_r, label="R Angular Velocity")
 
 plt.xlabel('Time (ms)')
-plt.title('IMU Update Speed 3 ms')
+plt.title('IMU Update Speed 500 ms')
 plt.legend()
 plt.show()
--- src/main.cpp
+++ src/main.cpp
@@ -8,25 +8,28 @@ static StarshotACSModelClass starshotObj;
 int iteration = 0;
 
 //ms
-int imu_delay = 3;
+int imu_delay = 500;
 float plantsim_step_size = 1;
 
-void setup(){  
+void setup()
+{
   //sqrt(x^2 + y^2 + z^2) < 5 degrees
-  plantObj.initialize(0.6,-0.5,0.7);
+  plantObj.initialize(0.6, -0.5, 0.7);
   starshotObj.initialize();
-  delay(10000); 
+  delay(10000);
 }
 
-void loop(){
+void loop()
+{
   starshotObj.step();
 
   plantObj.rtU.current[0] = starshotObj.rtY.detumble[0];
   plantObj.rtU.current[1] = starshotObj.rtY.detumble[1];
   plantObj.rtU.current[2] = starshotObj.rtY.detumble[2];
 
-  if(iteration%500 == 0){
-    Serial.print(iteration*imu_delay);
+  if (iteration % 1 == 0)
+  {
+    Serial.print(iteration * imu_delay);
     Serial.print(",");
     Serial.print(starshotObj.rtU.w[0]);
     Serial.print(",");
@@ -41,13 +44,11 @@ void loop(){
     Serial.println(plantObj.rtU.current[2]);
   }
 
-
-
-  for(int i = 0; i < imu_delay/plantsim_step_size; i++){
-    plantObj.step(plantsim_step_size/1000);
+  for (int i = 0; i < imu_delay / plantsim_step_size; i++)
+  {
+    plantObj.step(plantsim_step_size / 1000);
   }
 
-  
   starshotObj.rtU.w[0] = plantObj.rtY.angularvelocity[0];
   starshotObj.rtU.w[1] = plantObj.rtY.angularvelocity[1];
   starshotObj.rtU.w[2] = plantObj.rtY.angularvelocity[2];
@@ -57,4 +58,3 @@ void loop(){
 
   iteration++;
 }
-

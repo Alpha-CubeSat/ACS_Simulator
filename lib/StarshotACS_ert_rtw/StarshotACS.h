c779c5ffd73720a115b0470887f630d7c40ad24a
Natsoulas <95187192+Natsoulas@users.noreply.github.com>
2022-04-15 04:13:07 -0400

    added relevant changees to header file

--- lib/StarshotACS_ert_rtw/StarshotACS.h
+++ lib/StarshotACS_ert_rtw/StarshotACS.h
@@ -7,12 +7,12 @@
 //
 // Code generated for Simulink model 'StarshotACS'.
 //
-// Model version                  : 1.77
-// Simulink Coder version         : 9.1 (R2019a) 23-Nov-2018
-// C/C++ source code generated on : Sat Oct 23 12:00:04 2021
+// Model version                  : 10.15
+// Simulink Coder version         : 9.6 (R2021b) 14-May-2021
+// C/C++ source code generated on : Fri Mar 25 14:36:39 2022
 //
 // Target selection: ert.tlc
-// Embedded hardware selection: ARM Compatible->ARM Cortex
+// Embedded hardware selection: ARM Compatible->ARM Cortex-M
 // Code generation objectives:
 //    1. Execution efficiency
 //    2. RAM efficiency
@@ -21,67 +21,60 @@
 #ifndef RTW_HEADER_StarshotACS_h_
 #define RTW_HEADER_StarshotACS_h_
 #include <cmath>
-#ifndef StarshotACS_COMMON_INCLUDES_
-#define StarshotACS_COMMON_INCLUDES_
 #include "rtwtypes.h"
-#endif // StarshotACS_COMMON_INCLUDES_
+
+// Model Code Variants
 
 // Macros for accessing real-time model data structure
 #ifndef rtmGetErrorStatus
-#define rtmGetErrorStatus(rtm) ((rtm)->errorStatus)
+#define rtmGetErrorStatus(rtm)         ((rtm)->errorStatus)
 #endif
 
 #ifndef rtmSetErrorStatus
-#define rtmSetErrorStatus(rtm, val) ((rtm)->errorStatus = (val))
+#define rtmSetErrorStatus(rtm, val)    ((rtm)->errorStatus = (val))
 #endif
 
 // Forward declaration for rtModel
 typedef struct tag_RTM RT_MODEL;
 
 // Block signals and states (default storage) for system '<Root>'
-typedef struct
-{
-  real_T DiscreteTimeIntegrator_DSTATE[3]; // '<S2>/Discrete-Time Integrator'
-  real_T UD_DSTATE[3];                     // '<S5>/UD'
-  real_T UD_DSTATE_k;                      // '<S7>/UD'
-} DW;
+struct DW {
+  real_T DiscreteTimeIntegrator_DSTATE[3];// '<S2>/Discrete-Time Integrator'
+  real_T UD_DSTATE[3];                 // '<S5>/UD'
+  real_T UD_DSTATE_k;                  // '<S7>/UD'
+};
 
 // Invariant block signals (default storage)
-typedef const struct tag_ConstB
-{
-  real_T VectorConcatenate[9]; // '<S10>/Vector Concatenate'
-} ConstB;
+struct ConstB {
+  real_T VectorConcatenate[9];         // '<S10>/Vector Concatenate'
+};
 
 // Constant parameters (default storage)
-typedef struct
-{
+struct ConstP {
   // Expression: [1 0 0;0 1 0;0 0 1]
   //  Referenced by: '<S2>/Identity matrix'
 
   real_T Identitymatrix_Value[9];
-} ConstP;
-
-// External inputs (root inport signals with default storage)
-typedef struct
-{
-  real_T w[3];           // '<Root>/angularvelocity'
-  real_T Bfield_body[3]; // '<Root>/Bfield_body'
-} ExtU;
-
-// External outputs (root outports fed by signals with default storage)
-typedef struct
-{
-  real_T detumble[3]; // '<Root>/detumble'
-  real_T point[3];    // '<Root>/point'
-} ExtY;
-
-// Real-time Model Data Structure
-struct tag_RTM
-{
-  const char_T *volatile errorStatus;
 };
 
-extern const ConstB rtConstB; // constant block i/o
+// External inputs (root inport signals with default storage)
+struct ExtU {
+  real_T w[3];                         // '<Root>/angularvelocity'
+  real_T Bfield_body[3];               // '<Root>/Bfield_body'
+};
+
+// External outputs (root outports fed by signals with default storage)
+struct ExtY {
+  real_T detumble[3];                  // '<Root>/detumble'
+  real_T point[3];                     // '<Root>/point'
+};
+
+// Real-time Model Data Structure
+struct tag_RTM {
+  const char_T * volatile errorStatus;
+};
+
+extern const ConstB rtConstB;          // constant block i/o
 
 // Constant parameters (default storage)
 extern const ConstP rtConstP;
@@ -90,8 +83,9 @@ extern const ConstP rtConstP;
 class StarshotACSModelClass
 {
   // public data and function members
-public:
-  real_T pointing_error;
+ public:
+  // Real-Time Model get method
+  RT_MODEL * getRTM();
 
   // External inputs
   ExtU rtU;
@@ -99,8 +93,15 @@ public:
   // External outputs
   ExtY rtY;
 
+  // allocation of memory for kane damper control parameters c and Id
+  double damperc;
+  double Id;
+  //memory for magnetorquer ampfactor calcs
+  double expression_m;
+  double m_max;
+
   // model initialize function
-  void initialize();
+  void initialize(double kane_damper_c, double kaneId, double ampfactor);
 
   // model step function
   void step();
@@ -111,12 +112,9 @@ public:
   // Destructor
   ~StarshotACSModelClass();
 
-  // Real-Time Model get method
-  RT_MODEL *getRTM();
-
   // private data and function members
-private:
-  // Block signals and states
+ private:
+  // Block states
   DW rtDW;
 
   // Real-Time Model
@@ -127,8 +125,10 @@ private:
 //  These blocks were eliminated from the model due to optimizations:
 //
 //  Block '<S5>/Data Type Duplicate' : Unused code path elimination
+//  Block '<S2>/To Workspace' : Unused code path elimination
 //  Block '<S7>/Data Type Duplicate' : Unused code path elimination
 //  Block '<S2>/Gain 1' : Eliminated nontunable gain of 1
+
 
 //-
 //  The generated code includes comments that allow you to trace directly
@@ -142,28 +142,28 @@ private:
 //  MATLAB hilite_system command to trace the generated code back
 //  to the parent model.  For example,
 //
-//  hilite_system('StarshotACS0/StarshotACS')    - opens subsystem StarshotACS0/StarshotACS
-//  hilite_system('StarshotACS0/StarshotACS/Kp') - opens and selects block Kp
+//  hilite_system('starshotsimv5/StarshotACS')    - opens subsystem starshotsimv5/StarshotACS
+//  hilite_system('starshotsimv5/StarshotACS/Kp') - opens and selects block Kp
 //
 //  Here is the system hierarchy for this model
 //
-//  '<Root>' : 'StarshotACS0'
-//  '<S1>'   : 'StarshotACS0/StarshotACS'
-//  '<S2>'   : 'StarshotACS0/StarshotACS/KD_controller'
-//  '<S3>'   : 'StarshotACS0/StarshotACS/Orientation controller'
-//  '<S4>'   : 'StarshotACS0/StarshotACS/KD_controller/Cross'
-//  '<S5>'   : 'StarshotACS0/StarshotACS/KD_controller/Discrete Derivative'
-//  '<S6>'   : 'StarshotACS0/StarshotACS/KD_controller/Skew matrix S(m)1'
-//  '<S7>'   : 'StarshotACS0/StarshotACS/Orientation controller/Discrete Derivative'
-//  '<S8>'   : 'StarshotACS0/StarshotACS/Orientation controller/Norm2'
-//  '<S9>'   : 'StarshotACS0/StarshotACS/Orientation controller/Norm3'
-//  '<S10>'  : 'StarshotACS0/StarshotACS/Orientation controller/Skew matrix S(tau_c)1'
-//  '<S11>'  : 'StarshotACS0/StarshotACS/Orientation controller/Subsystem1'
-//  '<S12>'  : 'StarshotACS0/StarshotACS/Orientation controller/sign_function1'
-//  '<S13>'  : 'StarshotACS0/StarshotACS/Orientation controller/Subsystem1/Norm4'
-//  '<S14>'  : 'StarshotACS0/StarshotACS/Orientation controller/Subsystem1/Norm5'
+//  '<Root>' : 'starshotsimv5'
+//  '<S1>'   : 'starshotsimv5/StarshotACS'
+//  '<S2>'   : 'starshotsimv5/StarshotACS/KD_controller'
+//  '<S3>'   : 'starshotsimv5/StarshotACS/Orientation controller'
+//  '<S4>'   : 'starshotsimv5/StarshotACS/KD_controller/Cross'
+//  '<S5>'   : 'starshotsimv5/StarshotACS/KD_controller/Discrete Derivative'
+//  '<S6>'   : 'starshotsimv5/StarshotACS/KD_controller/Skew matrix S(m)1'
+//  '<S7>'   : 'starshotsimv5/StarshotACS/Orientation controller/Discrete Derivative'
+//  '<S8>'   : 'starshotsimv5/StarshotACS/Orientation controller/Norm2'
+//  '<S9>'   : 'starshotsimv5/StarshotACS/Orientation controller/Norm3'
+//  '<S10>'  : 'starshotsimv5/StarshotACS/Orientation controller/Skew matrix S(tau_c)1'
+//  '<S11>'  : 'starshotsimv5/StarshotACS/Orientation controller/Subsystem1'
+//  '<S12>'  : 'starshotsimv5/StarshotACS/Orientation controller/sign_function1'
+//  '<S13>'  : 'starshotsimv5/StarshotACS/Orientation controller/Subsystem1/Norm4'
+//  '<S14>'  : 'starshotsimv5/StarshotACS/Orientation controller/Subsystem1/Norm5'
 
-#endif // RTW_HEADER_StarshotACS_h_
+#endif                                 // RTW_HEADER_StarshotACS_h_
 
 //
 // File trailer for generated code.

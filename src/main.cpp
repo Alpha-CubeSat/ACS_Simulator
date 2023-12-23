42cf0f3f7a515dcd9937864fafcc8d7fe483240f
Deemo Chen <yc938@cornell.edu>
2023-12-22 22:13:02 -0500

    full setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,50 @@
 #include <Arduino.h>
 #include <math.h>
 #include <Wire.h>
-#include <Adafruit_LSM9DS1.h>
-#include <ArduinoEigen.h>
 
 #include "../lib/ACS_libs/StarshotACS_ert_rtw/StarshotACS.h"
-#include "../ekf/ekf.h"
-#include "DataLogging.hpp"
+#include "../lib/ACS_libs/Plant_ert_rtw/Plant.h"
+#include "../lib/ACS_libs/ekf/ekf.h"
 
-// Pins for all inputs, keep in mind the PWM defines must be on PWM pins
-#define AIN1 28
-#define AIN2 27
-#define PWMA 30
+
 #define LED 13
-#define file_name "100ms"
+
 
 static StarshotACS starshotObj;
+static Plant plantObj;
 static EKF ekfObj;
-Adafruit_LSM9DS1 imu = Adafruit_LSM9DS1();
 
-int current2PWM(float current)
-{
-  if (int(633.5 * pow(fabs(current), 0.6043) + 8.062) < 8.062)
-    return 0;
-  else if (int(633.5 * pow(fabs(current), 0.6043) + 8.062) > 255)
-    return 255;
-  else
-    return int(633.5 * pow(fabs(current), 0.6043) + 8.062);
-}
+/// PLANT PARAMETERS///
+double altitude_input = 400;
+double I_input[9] = {0.00195761450869, -5.836632382E-5, 2.27638093E-6,
+                     -5.836632382E-5, 0.00196346658902, 8.8920475E-7, 2.27638093E-6, 8.8920475E-7,
+                     0.00204697265884};
 
-// /// STARSHOT PARAMETERS///
+double inclination_input = 0.90058989402907408; // 51.6 deg in rad
+double m_input = 1.3;                           // kg
+double q0_input[4] = {0.5, 0.5, -0.18301270189221924, 0.6830127018922193};
+
+// ideally for pointing w xyz is [0,0,1], detumble test init w xyz could be, say, [ 0.008, -0.005, -0.0001] (total <5 deg/s)
+double wx_input = 0.0;
+double wy_input = 0.0;
+double wz_input = 1.0;
+
+// double wx_input = 0.008;
+// double wy_input = -0.005;
+// double wz_input = -0.0001;
+
+/// STARSHOT PARAMETERS///
 double A_input = 4.0E-5;
-double Id_input = 0.0021;
-
-double Kd_input = 0.5;
-double Kp_input = 3;
-
-double c_input = 0.004;
+double Id_input = 0.196;
+double Kd_input = 0.0007935279615795299;
+double Kp_input = 5.2506307629097953E-10;
+double c_input = 1.0E-5;
 double i_max_input = 0.25;
 double k_input = 13.5;
 double n_input = 500.0;
-double step_size_input = 0.1; // sec
-// /// INPUT DATA///
 
-
-
-void ACSWrite(float current)
-{
-
-  int PWM = current2PWM(current);
-
-  if (PWM == 0)
-  {
-    digitalWrite(AIN1, LOW);
-    digitalWrite(AIN2, LOW);
-  }
-
-  if (current < 0)
-  {
-    digitalWrite(AIN1, HIGH);
-    digitalWrite(AIN2, LOW);
-    analogWrite(PWMA, PWM);
-  }
-
-  if (current > 0)
-  {
-    digitalWrite(AIN1, LOW);
-    digitalWrite(AIN2, HIGH);
-    analogWrite(PWMA, PWM);
-  }
-}
+double plant_step_size_input = 0.001; //s
+double controller_step_size_input = 0.100; // s
 
 void setup()
 {
@@ -89,122 +64,89 @@ void setup()
       -7.75657503e-05, -1.26277622e-05, 5.67092127e-05, -6.07376965e-07, 2.97298687e-08, 8.52192033e-06;
   // Hd
   Eigen::MatrixXd Hd = Eigen::MatrixXd::Identity(6, 6);
-
   Serial.begin(9600);
 
-  DataLogSetup(file_name);
-  pinMode(LED, OUTPUT);
-  pinMode(AIN1, OUTPUT);
-  pinMode(AIN2, OUTPUT);
-  pinMode(PWMA, OUTPUT);
-
   digitalWrite(LED, HIGH);
-  starshotObj.initialize(step_size_input, A_input, Id_input, Kd_input, Kp_input, c_input, i_max_input, k_input, n_input);
-  ekfObj.initialize(step_size_input, initial_state, initial_cov, Q, Rd, Hd);
-  // DataLogSetup();
 
-  if (!imu.begin())
-  {
-    while (1)
-    {
-      Serial.println("wrong");
-      delay(100);
-    };
-  }
+  plantObj.initialize(plant_step_size_input, altitude_input, I_input, inclination_input, m_input, q0_input, wx_input, wy_input, wz_input);
+  starshotObj.initialize(controller_step_size_input, A_input, Id_input, Kd_input, Kp_input, c_input, i_max_input, k_input, n_input);
+  ekfObj.initialize(controller_step_size_input, initial_state, initial_cov, Q, Rd, Hd);
 
-  Serial.println("Setting up imu9DS1 9DOF");
-  imu.setupAccel(imu.LSM9DS1_ACCELRANGE_2G);
-  imu.setupMag(imu.LSM9DS1_MAGGAIN_8GAUSS);
-  imu.setupGyro(imu.LSM9DS1_GYROSCALE_245DPS);
+
 
   delay(1000);
 }
 
+int iter = 0;
 void loop()
 {
-  sensors_event_t accel, mag, gyro, temp;
-  imu.getEvent(&accel, &mag, &gyro, &temp);
 
-  double time = millis();
+  for (int i = 0; i < (int)(controller_step_size_input / plant_step_size_input); i++)
+  {
 
-  //Remap axis(rotate around x-axis by 90 deg) uT to T
-  ekfObj.Z(0) = mag.magnetic.x /10.0;
-  ekfObj.Z(1) = mag.magnetic.y /10.0;
-  ekfObj.Z(2) = mag.magnetic.z /10.0;
+    // plantObj.rtU.current[0] = starshotObj.rtY.detumble[0];
+    // plantObj.rtU.current[1] = starshotObj.rtY.detumble[1];
+    // plantObj.rtU.current[2] = starshotObj.rtY.detumble[2];
 
-  ekfObj.Z(3) = gyro.gyro.x;
-  ekfObj.Z(4) = gyro.gyro.y;
-  ekfObj.Z(5) = gyro.gyro.z;
+    plantObj.rtU.current[0] = starshotObj.rtY.point[0];
+    plantObj.rtU.current[1] = starshotObj.rtY.point[1];
+    plantObj.rtU.current[2] = starshotObj.rtY.point[2];
+
+    plantObj.step();
+  }
+  // uT to T
+  ekfObj.Z(0) = plantObj.rtY.magneticfield[0] * 1000000.0;
+  ekfObj.Z(1) = plantObj.rtY.magneticfield[1] * 1000000.0;
+  ekfObj.Z(2) = plantObj.rtY.magneticfield[2] * 1000000.0;
+
+  ekfObj.Z(3) = plantObj.rtY.angularvelocity[0];
+  ekfObj.Z(4) = plantObj.rtY.angularvelocity[1];
+  ekfObj.Z(5) = plantObj.rtY.angularvelocity[2];
 
   ekfObj.step();
 
+  //uT to T
+  starshotObj.rtU.Bfield_body[0] = ekfObj.state(0) / 1000000.0;
+  starshotObj.rtU.Bfield_body[1] = ekfObj.state(1) / 1000000.0;
+  starshotObj.rtU.Bfield_body[2] = ekfObj.state(2) / 1000000.0;
+
   starshotObj.rtU.w[0] = ekfObj.state(3);
-  starshotObj.rtU.w[1] = ekfObj.state(5);
-  starshotObj.rtU.w[2] = -ekfObj.state(4);
+  starshotObj.rtU.w[1] = ekfObj.state(4);
+  starshotObj.rtU.w[2] = ekfObj.state(5);
 
-  starshotObj.rtU.Bfield_body[0] = ekfObj.state(0) ;
-  starshotObj.rtU.Bfield_body[1] = ekfObj.state(2) ;
-  starshotObj.rtU.Bfield_body[2] = -ekfObj.state(1) ;
-
-  // starshotObj.rtU.w[0] = gyro.gyro.x;
-  // starshotObj.rtU.w[1] = gyro.gyro.z;
-  // starshotObj.rtU.w[2] = -gyro.gyro.y;
-
-  // starshotObj.rtU.Bfield_body[0] = mag.magnetic.x;
-  // starshotObj.rtU.Bfield_body[1] = mag.magnetic.z;
-  // starshotObj.rtU.Bfield_body[2] = -mag.magnetic.y;
-  //////
   starshotObj.step();
 
-  //test bench current adjust due to high B field
-  // double current_adjust = starshotObj.rtY.point[2] * 5.0;
-  ACSWrite(0.25);
-  // Serial.print(starshotObj.rtY.pt_error);
-  // Serial.print(", ");
-  // Serial.print(starshotObj.rtY.point[2]);
-  // Serial.print(", ");
-  // Serial.print(gyro.gyro.x);
-  // Serial.print(", ");
-  // Serial.print(gyro.gyro.y);
-  // Serial.print(", ");
-  // Serial.print(gyro.gyro.z);
-  // Serial.print(", ");
-  // Serial.print(mag.magnetic.x);
-  // Serial.print(", ");
-  // Serial.print(mag.magnetic.y);
-  // Serial.print(", ");
-  // Serial.print(mag.magnetic.z);
-  // Serial.print(", ");
-  // Serial.print(ekfObj.state(3));
-  // Serial.print(", ");
-  // Serial.print(ekfObj.state(4));
-  // Serial.print(", ");
-  // Serial.print(ekfObj.state(5));
-  // Serial.print(", ");
-  // Serial.print(ekfObj.state(0) );
-  // Serial.print(", ");
-  // Serial.print(ekfObj.state(1) );
-  // Serial.print(", ");
-  // Serial.print(ekfObj.state(2) );
-  // Serial.println();
+  // IF STEP THE PLANT THE LAST, ALL DATA WILL BE NAN !!!
+  // for (int i=0; i < (int)(controller_step_size_input / plant_step_size_input); i++)
+  // {
+  //   plantObj.rtU.current[0] = starshotObj.rtY.point[0];
+  //   plantObj.rtU.current[1] = starshotObj.rtY.point[1];
+  //   plantObj.rtU.current[2] = starshotObj.rtY.point[2];
 
-  double time_diff = millis()-time;
-  // Serial.print("Total ACS: ");
-  // Serial.println(time_diff);
-
-
-  // data
-  // int PWM = current2PWM(current_adjust);
-  double IMUData[15] = {starshotObj.rtY.point[2], starshotObj.rtY.pt_error, mag.magnetic.x, mag.magnetic.y, mag.magnetic.z, gyro.gyro.x, gyro.gyro.y, gyro.gyro.z, ekfObj.state(3), ekfObj.state(4), ekfObj.state(5), ekfObj.state(0), ekfObj.state(1), ekfObj.state(2)};
-  DataLog(IMUData, 15, file_name);
+  //   plantObj.step();
+  // }
   
-  //always taking 100 ms
-  int delay_time = 100 - time_diff;
-
-  if(delay_time<0){
-    Serial.printf("too slow! \n");
-    delay_time = 0;
-  }
-
-  delay(delay_time);
+  Serial.print(iter * controller_step_size_input); // time stamp
+  Serial.print(", ");
+  Serial.print(starshotObj.rtY.pt_error); //deg
+  Serial.print(", ");
+  Serial.print(plantObj.rtU.current[0]);
+  Serial.print(", ");
+  Serial.print(plantObj.rtU.current[1]);
+  Serial.print(", ");
+  Serial.print(plantObj.rtU.current[2]);
+  Serial.print(", ");
+  Serial.print(plantObj.rtY.magneticfield[0] * 1000000.0); //uT
+  Serial.print(", ");
+  Serial.print(plantObj.rtY.magneticfield[1] * 1000000.0); // uT
+  Serial.print(", ");
+  Serial.print(plantObj.rtY.magneticfield[2] * 1000000.0); // uT
+  Serial.print(", ");
+  Serial.print(plantObj.rtY.angularvelocity[0]);
+  Serial.print(", ");
+  Serial.print(plantObj.rtY.angularvelocity[1]);
+  Serial.print(", ");
+  Serial.print(plantObj.rtY.angularvelocity[2]);
+  Serial.println();
+  iter++;
 }

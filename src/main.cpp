48d067571417202231c4f900204e4675ed22a64a
od66 <117664865+od66@users.noreply.github.com>
2025-01-20 20:44:30 -0500

    Update main.cpp to apply EKF

--- src/main.cpp
+++ src/main.cpp
@@ -2,178 +2,173 @@
 #include <math.h>
 #include <Wire.h>
 #include <Adafruit_LSM9DS1.h>
-
+#include <ArduinoEigen.h>
+#include "CalibratedEKF.hpp"  // Include our new EKF class
 #include "../lib/ACS_libs/StarshotACS_ert_rtw/StarshotACS.h"
-
 #include "DataLogging.hpp"
 
-// Pins for all inputs, keep in mind the PWM defines must be on PWM pins, 2.29.24 Note that some of the pins were re-arranged to make a cleaner setup
+// Pin definitions
 #define AIN1 31
 #define AIN2 32
 #define PWMA 30
 #define STBY 29
 #define LED 13
-#define file_name "test"
+#define FILE_NAME "test"
+#define VOLTAGE_PIN 23
 
+// Global objects
 static StarshotACS starshotObj;
 Adafruit_LSM9DS1 imu = Adafruit_LSM9DS1();
+CalibratedEKF ekf;
 
-int current2PWM(float current)
-{
-  if (int(633.5 * pow(fabs(current), 0.6043) + 8.062) < 8.062)
-    return 0;
-  else if (int(633.5 * pow(fabs(current), 0.6043) + 8.062) > 255)
-    return 255;
-  else
-    return int(633.5 * pow(fabs(current), 0.6043) + 8.062);
+// Parameters
+const double step_size = 0.2;  // seconds
+const int DELAY_MS = 100;
+
+// Convert current to PWM value
+int current2PWM(float current) {
+    if (int(633.5 * pow(fabs(current), 0.6043) + 8.062) < 8.062)
+        return 0;
+    else if (int(633.5 * pow(fabs(current), 0.6043) + 8.062) > 255)
+        return 255;
+    else
+        return int(633.5 * pow(fabs(current), 0.6043) + 8.062);
 }
 
-// /// STARSHOT PARAMETERS///
-double A_input = 4.0E-5;
-double Id_input = 0.0021;
+// Write current to ACS
+void ACSWrite(float current) {
+    int PWM = current2PWM(current);
 
-double Kd_input = 0.0001;
-double Kp_input = 5;
-
-double c_input = 0.004;
-double i_max_input = 0.25;
-double k_input = 13.5;
-double n_input = 500.0;
-double step_size_input = 0.2; // sec
-// /// INPUT DATA///
-
-void ACSWrite(float current)
-{
-
-  int PWM = current2PWM(current);
-
-  if (PWM == 0)
-  {
-    digitalWrite(AIN1, LOW);
-    digitalWrite(AIN2, LOW);
-  }
-
-  if (current < 0)
-  {
-    digitalWrite(AIN1, HIGH);
-    digitalWrite(AIN2, LOW);
-    analogWrite(PWMA, PWM);
-  }
-
-  if (current > 0)
-  {
-    digitalWrite(AIN1, LOW);
-    digitalWrite(AIN2, HIGH);
-    analogWrite(PWMA, PWM);
-  }
+    if (PWM == 0) {
+        digitalWrite(AIN1, LOW);
+        digitalWrite(AIN2, LOW);
+    } else if (current < 0) {
+        digitalWrite(AIN1, HIGH);
+        digitalWrite(AIN2, LOW);
+        analogWrite(PWMA, PWM);
+    } else {
+        digitalWrite(AIN1, LOW);
+        digitalWrite(AIN2, HIGH);
+        analogWrite(PWMA, PWM);
+    }
 }
 
-void setup()
-{
-  Serial.begin(9600);
+float readBatteryVoltage() {
+    const float voltage_ref = 3.3;
+    const int resolution = 1023;
+    const int r1 = 4700;
+    const int r2 = 10000;
+    
+    return analogRead(VOLTAGE_PIN) * voltage_ref / resolution * (r1 + r2) / r2;
+}
 
-  DataLogSetup(file_name);
-  pinMode(LED, OUTPUT);
-  pinMode(AIN1, OUTPUT);
-  pinMode(AIN2, OUTPUT);
-  pinMode(PWMA, OUTPUT);
+void setup() {
+    Serial.begin(9600);
+    DataLogSetup(FILE_NAME);
 
-  digitalWrite(LED, HIGH);
-  starshotObj.initialize(step_size_input, A_input, Id_input, Kd_input, Kp_input, c_input, i_max_input, k_input, n_input);
+    // Pin setup
+    pinMode(LED, OUTPUT);
+    pinMode(AIN1, OUTPUT);
+    pinMode(AIN2, OUTPUT);
+    pinMode(PWMA, OUTPUT);
+    digitalWrite(LED, HIGH);
 
-  // DataLogSetup();
+    // Initialize Starshot
+    starshotObj.initialize(step_size, 4.0E-5, 0.0021, 0.0001, 5, 0.004, 0.25, 13.5, 500.0);
 
-  if (!imu.begin())
-  {
-    while (1)
-    {
-      Serial.println("wrong");
-      delay(100);
+    // Initialize IMU
+    if (!imu.begin()) {
+        while (1) {
+            Serial.println("IMU initialization failed");
+            delay(100);
+        }
+    }
+
+    Serial.println("Initializing IMU and EKF...");Serial.println("Setting up LSM9DS1 9DOF IMU...");
+    // Configure IMU
+    imu.setupAccel(imu.LSM9DS1_ACCELRANGE_2G);
+    imu.setupMag(imu.LSM9DS1_MAGGAIN_12GAUSS);
+    imu.setupGyro(imu.LSM9DS1_GYROSCALE_245DPS);
+
+    // Initialize EKF
+    Eigen::VectorXd initial_state(6);
+    initial_state.setZero();
+
+    Eigen::MatrixXd initial_covariance = Eigen::MatrixXd::Identity(6, 6) * 0.1;
+    Eigen::MatrixXd process_noise = Eigen::MatrixXd::Identity(6, 6) * 0.01;
+    Eigen::MatrixXd measurement_noise = Eigen::MatrixXd::Identity(6, 6) * 0.1;
+    Eigen::MatrixXd H = Eigen::MatrixXd::Identity(6, 6);  // Measurement matrix
+
+    ekf.initialize(step_size, initial_state, initial_covariance, 
+                  process_noise, measurement_noise, H, readBatteryVoltage());
+
+    delay(1000);
+}
+
+void loop() {
+    unsigned long start = millis();
+
+    // Read sensor data
+    sensors_event_t accel, mag, gyro, temp;
+    imu.getEvent(&accel, &mag, &gyro, &temp);
+
+    // Read battery voltage and update EKF calibration
+    float voltage = readBatteryVoltage();
+    ekf.updateVoltage(voltage);
+
+    // Create measurement vector
+    Eigen::VectorXd measurement(6);
+    
+    // Hard iron corrections
+    float mag_x = mag.magnetic.x - (-4.9547000000000025);
+    float mag_y = mag.magnetic.z - (-13.855600000000003);
+    float mag_z = -mag.magnetic.y - (49.75155);
+
+    float gyro_x = gyro.gyro.x - 0.07280736884261114;
+    float gyro_y = gyro.gyro.z - 0.016019223067681217;
+    float gyro_z = -gyro.gyro.y - 0.020224269122947534;
+
+    // Update EKF measurement
+    measurement << mag_x, mag_y, mag_z, gyro_x, gyro_y, gyro_z;
+    ekf.Z = measurement;
+
+    // Perform EKF step
+    ekf.step();
+
+    // Get corrected state for control
+    Eigen::VectorXd corrected_state = ekf.state;
+    
+    // Update Starshot with corrected measurements
+    starshotObj.rtU.Bfield_body[0] = corrected_state(0);
+    starshotObj.rtU.Bfield_body[1] = corrected_state(1);
+    starshotObj.rtU.Bfield_body[2] = corrected_state(2);
+    starshotObj.rtU.w[0] = corrected_state(3);
+    starshotObj.rtU.w[1] = corrected_state(4);
+    starshotObj.rtU.w[2] = corrected_state(5);
+
+    // Run Starshot control step
+    starshotObj.step();
+
+    // Apply control output
+    double current_adjust = starshotObj.rtY.point[2] * 5.0;
+    ACSWrite(current_adjust);
+
+    // Log data
+    int PWM = current2PWM(current_adjust);
+    double IMUData[7] = {
+        starshotObj.rtY.pt_error,
+        current_adjust,
+        static_cast<double>(PWM),
+        corrected_state(0),  // Corrected magnetic field measurements
+        corrected_state(1),
+        corrected_state(2),
+        voltage
     };
-  }
+    DataLog(IMUData, 7, FILE_NAME);
 
-  Serial.println("Setting up imu9DS1 9DOF");
-  imu.setupAccel(imu.LSM9DS1_ACCELRANGE_2G);
-  imu.setupMag(imu.LSM9DS1_MAGGAIN_12GAUSS); // Applying 5V on the large coils will make the magnetometer max out at around 1000 uT
-  imu.setupGyro(imu.LSM9DS1_GYROSCALE_245DPS);
-
-  delay(1000);
-}
-
-void loop()
-{
-
-  unsigned long start = millis();
-
-  sensors_event_t accel, mag, gyro, temp;
-  imu.getEvent(&accel, &mag, &gyro, &temp);
-
-  int pwm_y = 0; // will be re-assigned to the previous PWM value, previous PWM value needs to be used to calculate the current offset
-
-  // Remap axis(rotate around x-axis by 90 deg)
-
-  // Calibrating and assigning magnetometer values
-  float mag_hardiron_x = -4.9547000000000025;
-  float mag_hardiron_y = 49.75155;
-  float mag_hardiron_z = -13.855600000000003;
-
-  float pwmY_ox_1 = 8.83096680e-03;
-  float pwmY_ox_2 = 4.26409072e-07;
-  float pwmY_ox_3 = -6.69370023e-09;
-  float pwmY_oy_1 = -2.64514092e-01;
-  float pwmY_oy_2 = -9.82458813e-06;
-  float pwmY_oy_3 = 9.11136691e-08;
-  float pwmY_oz_1 = -1.90567242e-02;
-  float pwmY_oz_2 = -5.99945842e-06;
-  float pwmY_oz_3 = 7.85718685e-10;
-
-  // Change all naming to oop-flight-code naming
-  float pwmY_ox = (pwmY_ox_1 * pwm_y) + (pwmY_ox_2 * pow(pwm_y, 2)) + (pwmY_ox_3 * pow(pwm_y, 3));
-  float pwmY_oy = (pwmY_oy_1 * pwm_y) + (pwmY_oy_2 * pow(pwm_y, 2)) + (pwmY_oy_3 * pow(pwm_y, 3));
-  float pwmY_oz = (pwmY_oz_1 * pwm_y) + (pwmY_oz_2 * pow(pwm_y, 2)) + (pwmY_oz_3 * pow(pwm_y, 3));
-
-  starshotObj.rtU.Bfield_body[0] = mag.magnetic.x - mag_hardiron_x - pwmY_ox;
-  starshotObj.rtU.Bfield_body[1] = mag.magnetic.z - mag_hardiron_z - pwmY_oz;
-  starshotObj.rtU.Bfield_body[2] = -mag.magnetic.y - mag_hardiron_y - pwmY_oy;
-
-  // Calibrating and assigning gyro values
-  float gyro_hardiron_x = 0.07280736884261114;
-  float gyro_hardiron_y = 0.020224269122947534;
-  float gyro_hardiron_z = 0.016019223067681217;
-
-  starshotObj.rtU.w[0] = gyro.gyro.x - gyro_hardiron_x;
-  starshotObj.rtU.w[1] = gyro.gyro.z - gyro_hardiron_z;
-  starshotObj.rtU.w[2] = -gyro.gyro.y - gyro_hardiron_y;
-
-  // Getting battery voltage
-  int voltage_value_pin = 23;
-  float voltage_ref = 3.3;
-  int resolution = 1023;
-  int r1 = 4700;
-  int r2 = 10000;
-
-  float voltage = analogRead(voltage_value_pin) * voltage_ref / resolution * (r1 + r2) / r2;
-
-  // starshotObj.rtU.w[0] = gyro.gyro.x;
-  // starshotObj.rtU.w[1] = gyro.gyro.z;
-  // starshotObj.rtU.w[2] = -gyro.gyro.y;
-
-  // starshotObj.rtU.Bfield_body[0] = mag.magnetic.x;
-  // starshotObj.rtU.Bfield_body[1] = mag.magnetic.z;
-  // starshotObj.rtU.Bfield_body[2] = -mag.magnetic.y;
-  starshotObj.step();
-
-  // test bench current adjust due to high B field
-  double current_adjust = starshotObj.rtY.point[2] * 5.0;
-  ACSWrite(current_adjust);
-
-  // data
-  int PWM = current2PWM(current_adjust);
-  pwm_y = PWM;
-  double IMUData[7] = {starshotObj.rtY.pt_error, current_adjust, PWM, starshotObj.rtU.Bfield_body[0], starshotObj.rtU.Bfield_body[2], starshotObj.rtU.Bfield_body[1], voltage};
-  DataLog(IMUData, 7, file_name);
-  Serial.println("data logged");
-
-  unsigned long end = millis();
-  delay(100 - (end - start));
+    // Maintain loop timing
+    unsigned long end = millis();
+    if (end - start < DELAY_MS) {
+        delay(DELAY_MS - (end - start));
+    }
 }
